b420a0fb9093139e1fca17d6465b35ef6f9a1713
junpei10 <miurajunpeis2@gmail.com>
2021-11-24 07:26:12 +0900

    refactor: css class name &  efficiency of script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, ViewEncapsulation } from '@angular/core';
+import { MediaQuery } from './services/media-query.service';
+
+import { RootRouteNames } from './app-routing.module';
+import { RouteChanges, ROUTE_CHANGES } from './services/route-changes.service';
+import { YoungestRoute } from './services/youngest-route.service';
+import { Meta } from './services/meta.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'elextbook';
+  title = 'Elextbook';
+
+  private _doCheckCount = 0;
+
+  mediaQueryState: {
+    isPC: boolean;
+    headerLeftButtonVariant: 'icon' | 'basic';
+    navTrackerPosition: 'after' | 'before';
+    navTrackerOrientation: 'vertical' | 'horizontal';
+  } = {} as any;
+
+  selectedRoute: { [route in RootRouteNames]?: 'primary' } = {};
+  selectedRouteIndex: number | undefined;
+
+  constructor(
+    changeDetector: ChangeDetectorRef,
+    mediaQuery: MediaQuery,
+    meta: Meta,
+    youngestRoute: YoungestRoute,
+    @Inject(ROUTE_CHANGES) routeChanges: RouteChanges,
+  ) {
+    routeChanges.subscribe(() => {
+      const data = youngestRoute.snapshot.data;
+      console.log(data);
+
+      meta.update(data);
+      const rootData = data.root;
+      if (rootData) {
+        this.selectedRouteIndex = rootData.index;
+        this.selectedRoute = {
+          [rootData.key]: 'primary'
+        };
+
+      } else {
+        this.selectedRouteIndex = void 0;
+        this.selectedRoute = {}
+      }
+    });
+
+    mediaQuery.pcChanges.subscribe(isPC => {
+      if (isPC) {
+        this.mediaQueryState = {
+          isPC: true,
+          headerLeftButtonVariant: 'basic',
+          navTrackerPosition: 'after',
+          navTrackerOrientation: 'vertical'
+        };
+
+      } else {
+        this.mediaQueryState = {
+          isPC: false,
+          headerLeftButtonVariant: 'icon',
+          navTrackerPosition: 'before',
+          navTrackerOrientation: 'horizontal'
+        };
+      }
+
+      changeDetector.markForCheck(); // 初回ロード時に発火するが、レンダリング回数に影響しないことを検証済み
+    });
+  }
+
+  ngDoCheck(): void {
+    this._doCheckCount++;
+    console.log('Do Check', this._doCheckCount);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { MlButtonModule } from '@material-lite/angular/button';
+import { MlRippleModule  } from '@material-lite/angular/core';
+import { MlStraightTrackerModule } from '@material-lite/angular-cdk/straight-tracker';
+
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MlRippleModule,
+    MlButtonModule,
+    MlStraightTrackerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
