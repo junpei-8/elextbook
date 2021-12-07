7db9c7b634fc917835cd948cddd9cead996e9223
junpei10 <miurajunpeis2@gmail.com>
2021-12-08 06:45:16 +0900

    add: injection for indexed db

---
+++ src/app/services/indexed-db.service.ts
@@ -0,0 +1,20 @@
+// import { Injectable } from '@angular/core';
+// import { InjectionToken } from "@angular/core";
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class IndexedDB {
+//   hasSupported: boolean;
+
+//   request: IDBOpenDBRequest;
+
+//   constructor() {
+//     const IDB = window.indexedDB;
+
+//     const hasSupported = this.hasSupported = !!IDB;
+//     if (!hasSupported) return;
+
+//     this.request = IDB.open('elextbook');
+//   }
+// }
