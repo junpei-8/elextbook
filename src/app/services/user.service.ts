234e15cd3fadba2b3376956e2c212ac418801592
junpei10 <miurajunpeis2@gmail.com>
2022-01-15 23:06:41 +0900

    fix: change detecotr

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,29 +1,29 @@
-import { Inject, Injectable, NgZone } from '@angular/core';
+import { ChangeDetectorRef, Inject, Injectable, NgZone, SkipSelf } from '@angular/core';
 import { ref, remove } from '@firebase/database';
 import { deleteUser, User as AuthUser } from 'firebase/auth';
 import { BehaviorSubject, filter, Observable } from 'rxjs';
 import { FIREBASE, Firebase } from './firebase';
 
-type UserState = AuthUser | null | false;
+type UserState = AuthUser | null | undefined;
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
-  state: UserState = false;
+  state: UserState = void 0;
 
   private _changesSubject: BehaviorSubject<UserState>;
   changes: Observable<UserState>;
 
   constructor(
     private _ngZone: NgZone,
-    @Inject(FIREBASE) private _firebase: Firebase
+    @Inject(FIREBASE) private _firebase: Firebase,
   ) {
     const auth = _firebase.auth;
     auth.onAuthStateChanged(this._onAuthStateChanged.bind(this));
 
-    const subject = this._changesSubject = new BehaviorSubject<any>(false);
-    this.changes = subject.pipe(filter(state => state !== false));
+    const subject = this._changesSubject = new BehaviorSubject<any>(void 0);
+    this.changes = subject.pipe(filter(state => state !== void 0));
   }
 
   private _onAuthStateChanged(state: AuthUser | null): void {
@@ -34,17 +34,16 @@ export class User {
   signOut(): void {
     this._ngZone.runOutsideAngular(() => {
       this._firebase.auth.signOut();
-      this._ngZone.run(() => {});
     })
   }
 
   delete(): void {
     const state = this.state;
+
     if (state) {
       this._ngZone.runOutsideAngular(() => {
         deleteUser(state);
         remove(ref(this._firebase.realtimeDB, 'users/' + state.uid));
-        this._ngZone.run(() => {});
       })
     }
   }
