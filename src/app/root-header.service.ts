932bd67abc00ede1671d661fc6f895b6b3ff5393
junpei10 <miurajunpeis2@gmail.com>
2022-01-15 23:02:22 +0900

    add: displaying user icon from User DB

--- src/app/root-header.service.ts
+++ src/app/root-header.service.ts
@@ -2,6 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable, NgZone } from '@angular/core';
 import { MlPortalContent } from '@material-lite/angular-cdk/portal';
 import { Fragment } from './services/fragment.service';
+import { User } from './services/user.service';
 
 interface SuggestContent {
   title: string;
@@ -30,12 +31,14 @@ const SVG_ICON_PATH = {
   star: 'M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z',
   clear: 'M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z',
   drawer: 'M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z',
-  people: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z',
+  people: 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjRweCIgdmlld0JveD0iMCAwIDI0IDI0IiB3aWR0aD0iMjRweCIgZmlsbD0iIzAwMDAwMCI+PHBhdGggZD0iTTAgMGgyNHYyNEgwVjB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTEyIDJDNi40OCAyIDIgNi40OCAyIDEyczQuNDggMTAgMTAgMTAgMTAtNC40OCAxMC0xMFMxNy41MiAyIDEyIDJ6TTcuMDcgMTguMjhjLjQzLS45IDMuMDUtMS43OCA0LjkzLTEuNzhzNC41MS44OCA0LjkzIDEuNzhDMTUuNTcgMTkuMzYgMTMuODYgMjAgMTIgMjBzLTMuNTctLjY0LTQuOTMtMS43MnptMTEuMjktMS40NWMtMS40My0xLjc0LTQuOS0yLjMzLTYuMzYtMi4zM3MtNC45My41OS02LjM2IDIuMzNDNC42MiAxNS40OSA0IDEzLjgyIDQgMTJjMC00LjQxIDMuNTktOCA4LThzOCAzLjU5IDggOGMwIDEuODItLjYyIDMuNDktMS42NCA0Ljgzek0xMiA2Yy0xLjk0IDAtMy41IDEuNTYtMy41IDMuNVMxMC4wNiAxMyAxMiAxM3MzLjUtMS41NiAzLjUtMy41UzEzLjk0IDYgMTIgNnptMCA1Yy0uODMgMC0xLjUtLjY3LTEuNS0xLjVTMTEuMTcgOCAxMiA4czEuNS42NyAxLjUgMS41UzEyLjgzIDExIDEyIDExeiIvPjwvc3ZnPg==',
   download: 'M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z',
   starOutline: 'M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z',
 };
 
 type SVGIconPathKeys = keyof typeof SVG_ICON_PATH;
+type SVGIconDirection = 'left' | 'right';
+type SVGContent = SVGPathElement | SVGImageElement;
 
 @Injectable({
   providedIn: 'root'
@@ -61,10 +64,15 @@ export class RootHeader {
 
   noop = noop;
 
+
+  unsubscribeUserChanges: () => void = this.noop ;
+
+
   constructor(
+    private _user: User,
     private _ngZone: NgZone,
     private _fragment: Fragment,
-    @Inject(DOCUMENT) private _document: Document
+    @Inject(DOCUMENT) private _document: Document,
   ) {}
 
   updateContents(): void {
@@ -128,7 +136,8 @@ export class RootHeader {
     this.classes[2] = isEnable ? 'will-change' : null;
   }
 
-  setActionIcon(direction: 'left' | 'right', key: SVGIconPathKeys | null, imagePath?: string): void {
+
+  setActionIcon(direction: SVGIconDirection, key: SVGIconPathKeys): void {
     let svgEl: HTMLElement;
 
     if (direction === 'left') {
@@ -142,28 +151,74 @@ export class RootHeader {
       else { this.rightActionSVGKey = key; };
     }
 
-    let enterPathEl: SVGImageElement | SVGPathElement;
-    if (imagePath) {
-      enterPathEl = this._document.createElementNS('http://www.w3.org/2000/svg', 'image');
-      enterPathEl.setAttribute('href', imagePath);
-      enterPathEl.classList.add('rh-icon-img');
+    if (key === 'people') {
+      const userIconURL = this._user.state?.photoURL;
+      svgEl.classList.value = 'rh-people-icon';
+
+      if (userIconURL) {
+        const img = this._createSvgImage(userIconURL);
+        this._attachSvgContent(svgEl, img, direction);
+        this.unsubscribeUserChanges();
+        this.unsubscribeUserChanges = noop;
+
+
+      } else {
+        const peoplePath = SVG_ICON_PATH.people;
+        const img = this._createSvgImage('data:image/svg+xml;base64,' + peoplePath);
+        this._attachSvgContent(svgEl, img, direction);
+
+        const subscription = this._user.changes
+          .subscribe((state) => {
+            const url = state?.photoURL;
+            if (url) {
+              const img = this._createSvgImage(url);
+              this._attachSvgContent(svgEl, img, direction);
+
+            } else {
+              const img = this._createSvgImage('data:image/svg+xml;base64,' + peoplePath);
+              this._attachSvgContent(svgEl, img, direction);
+            }
+          })
+
+        this.unsubscribeUserChanges();
+        this.unsubscribeUserChanges = () => subscription.unsubscribe();
+      }
+
 
     } else {
-      enterPathEl = this._document.createElementNS('http://www.w3.org/2000/svg', 'path') as any;
-      enterPathEl.setAttribute('d', SVG_ICON_PATH[key!]);
-    }
+      const el = this._document.createElementNS('http://www.w3.org/2000/svg', 'path') as any;
+      svgEl.classList.value = '';
 
-    const leavePathEl = svgEl.lastChild as HTMLElement;
+      el.setAttribute('d', SVG_ICON_PATH[key]);
+      this._attachSvgContent(svgEl, el, direction);
+      this.unsubscribeUserChanges();
+      this.unsubscribeUserChanges = noop;
+    }
+  }
+
+
+  private _attachSvgContent(svgElement: HTMLElement, content: SVGContent, direction: SVGIconDirection): void {
+    const leavePathEl = svgElement.lastChild as HTMLElement;
     if (leavePathEl)
       leavePathEl.classList.value = 'rh-' + direction + '-icon-path';
 
-    const enterPathClassList = enterPathEl.classList;
+    const enterPathClassList = content.classList;
     enterPathClassList.add('rh-' + direction + '-icon-path');
-    svgEl.appendChild(enterPathEl);
+    svgElement.appendChild(content);
+
     this._ngZone.runOutsideAngular(() =>
       setTimeout(() => enterPathClassList.add('rh-visible-' + direction + '-icon-path'), 16));
 
     this._ngZone.runOutsideAngular(() =>
       setTimeout(() => { if (leavePathEl) leavePathEl.remove(); }, 280));
+  }
+
+
+  private _createSvgImage(url: string): SVGImageElement {
+    const el = this._document.createElementNS('http://www.w3.org/2000/svg', 'image');
+    el.setAttribute('href', url);
+    el.classList.value = 'rh-icon';
+
+    return el;
   }
 }
